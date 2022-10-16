82122aa19287fdbd0685123058010516d4dd6700
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2022-10-16 20:00:50 +0530

    Fixed the Typo

--- Cados/settings.py
+++ Cados/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_URL = "staticfiles"
+STATIC_ROOT = "staticfiles"
 
 MEDIA_URL = f"{WEBHOST}/media/"
 MEDIA_ROOT = BASE_DIR / "media"
